73d543af774e1a312a8cdeb7e20ca7493234b46f
osadolor ebhuoma <ebhuoma@sheridancollege.ca>
2021-10-27 15:36:23 -0400

    This is the 2nd new Commit

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -11,10 +11,11 @@ package studentlist;
  */
 public class studentList {
     public static void main(String[] args){
-        Student[] studentList = new Student[3];
+        // added commit at 3:18pm
+        Student[] studentList = new Student[4];
         studentList[0] = new Student("1","Ronak Sheth");
         studentList[1] = new Student("2","John Doe");
-        studentList[2] = new Student("3","abc");
+        studentList[2] = new Student("3","Osadolor Ebhuoma");
         
         for(int  i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getStudentName());
